5a45c66e5ca346464391cade8d44786d0b7ed595
icarobernard <icarobernar@hotmail.com>
2022-12-08 15:50:24 -0300

    first commit

---
+++ src/api/atestado/controllers/atestado.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * atestado controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::atestado.atestado');
---
+++ src/api/atestado/services/atestado.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * atestado service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::atestado.atestado');
